9c4a89d439f77badcb28686082e82998d9c81ef2
Ashutosh312000 <95929238+Ashutosh312000@users.noreply.github.com>
2022-09-02 11:21:04 +0530

    Task:addbytagname

---
+++ dom.js
@@ -0,0 +1,7 @@
+//by classname
+var item5=document.getElementsByClassName('diffname');
+item5[0].style.fontWeight='bold'
+//by tag name
+var itemli=document.getElementsByTagName('li');
+itemli[4].style.color='pink';
+

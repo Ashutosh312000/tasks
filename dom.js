4c3c46e0509fae9c6bd403174e746012c59bb2f7
Ashutosh312000 <95929238+Ashutosh312000@users.noreply.github.com>
2022-09-02 12:18:02 +0530

    delete

--- dom.js
+++
@@ -1,6 +0,0 @@
-var item2=document.querySelector('.list-group-item:nth-child(2)');
-item2.style.backgroundColor='green';
-var item3=document.querySelector('.list-group-item:nth-child(3)');
-item3.style.display='none';
-
-

62103aaad5e6d44945a8a1f7deabf60b67e763e0
Ashutosh312000 <95929238+Ashutosh312000@users.noreply.github.com>
2022-09-02 17:29:09 +0530

    Delete dom.js

--- dom.js
+++
@@ -1,9 +0,0 @@
-var allitem2=document.querySelectorAll('.list-group-item'); 
-allitem2[1].style.color='green';
-
-var odd=document.querySelectorAll('li:nth-child(odd)');
-
-for(var  i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green';
-}
-

e66491df60838dbf86dcdd9f01cad14540788ed0
Ashutosh312000 <95929238+Ashutosh312000@users.noreply.github.com>
2022-10-10 10:15:58 +0530

    Update prp2.js

--- prp2.js
+++ prp2.js
@@ -1,12 +1,12 @@
 //Currying using bind
 let multiply=function(x,y){
-    console.log(x*y);
+    console.log(x*y); 
 }
 let multiplybytwo=multiply.bind(this,2);
-multiplybytwo(3);
+multiplybytwo(3); //output is 6
 
 let multiplybythree=multiply.bind(this,2,3);
-multiplybythree();
+multiplybythree(); //output is 6
 
 //Currying using closures
 let multiply2=function(x){
@@ -15,4 +15,4 @@ let multiply2=function(x){
     }
 }
 var multiplybyfour=multiply2(4);
-multiplybyfour(3);
+multiplybyfour(3); //output is 12

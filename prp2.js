165879638979460aa61af98c335bc447fc75758f
Ashutosh312000 <95929238+Ashutosh312000@users.noreply.github.com>
2022-10-25 15:01:00 +0530

    Task: Promise ans 4

---
+++ prp2.js
@@ -0,0 +1,64 @@
+const posts=[
+    {title:'post one',body:'this is post one'},
+    {title:'post two',body:'this is post two'},
+    {title:'post three',body:'this is post three'},
+];
+function getpost(){
+    setTimeout(()=>{
+        let output='';
+        posts.forEach((post,index)=>{
+            output+=`<li>${post.title}</li>`;
+        });
+        document.body.innerHTML=output;
+    },1)
+}
+function createpost(post){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            posts.push(post);
+            const error=false;
+
+            if(!error){
+                resolve('new post added');
+            }
+            else{
+                reject('Error: Something went wrong')
+            }
+        },1);
+    });
+   
+}
+function deletepost(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            
+            if(posts.length!=0){
+                posts.pop();
+                resolve('last post is deleted');
+            }
+            else{
+                reject('Error: Inside catch Array is empty now')
+            }
+            
+        },1000);
+    });
+   
+}
+
+createpost({title:'post four',body:'this is post four'})
+.then(()=>{
+    getpost();
+    deletepost().then(()=>{
+        getpost();
+    })
+})
+
+
+
+        
+
+
+
+
+
+

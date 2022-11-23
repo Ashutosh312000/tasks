22fc6ca809609e7786679fd82ad220a80df1e040
Ashutosh312000 <95929238+Ashutosh312000@users.noreply.github.com>
2022-11-23 11:08:19 +0530

    Task the booking appointment app
    this is the real one

---
+++ project2/routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const adminController = require('../controllers/admin');
+
+const router = express.Router();
+
+router.get('/',adminController.getmainpage);
+
+
+router.delete('/deleteAddProduct/:UserId', adminController.deleteAddProduct);
+
+
+router.post('/postAddProduct', adminController.postAddProduct);
+
+module.exports = router;

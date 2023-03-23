9a3a8de86c2e8da0992f628d2623d39e64bcc5e0
Ashutosh312000 <95929238+Ashutosh312000@users.noreply.github.com>
2023-03-23 11:08:05 +0530

    Task 4-: What are callbacks?

---
+++ prc.js
@@ -0,0 +1,31 @@
+const posts=[
+    {title:'post one',body:'this is post one',createAt:new Date().getTime()},
+    {title:'post two',body:'this is post two',createAt:new Date().getTime()}
+];
+// let intervalId=0;
+function getpost(){
+    // clearInterval(intervalId);
+    intervalId=setInterval(()=>{
+        let output='';
+        for(let i=0;i<posts.length;i++){
+            output+=`<li>${posts[i].title}  last updated ${(new Date().getTime() - posts[i].createAt) /1000}  seconds ago</li>`;
+        }
+        document.body.innerHTML=output;
+    },1000)
+}
+function createpost(post,callback){
+    setTimeout(()=>{
+        posts.push({...post,createAt:new Date().getTime()})
+        callback();
+    },2000);
+}
+// getpost();
+// createpost({title:'post three',body:'this is post three'},getpost);
+
+function create4thpost(post,callback){
+    setTimeout(()=>{
+        posts.push({...post,createAt:new Date().getTime()});
+        callback({title:'post three',body:'this is post three'},getpost);
+    },2000);
+}
+create4thpost({title:'post four',body:'this is post four'},createpost);
